e210e4a47768cad9ea66c96568b3ccd7a968c49f
Dave Hale <davejlhale@yahoo.co.uk>
2022-11-11 18:16:26 +0000

    css onevent anim playabout

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ console.log("Hello There!");
 let buttons = document.querySelectorAll("#buttons div");
 playSound = (ev) =>{
     new Audio("sounds/" + ev.id + ".wav").play();
-    ev.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
-    ev.style.boxShadow = "0px 0px 30px rgb(255, 255, 255)";
+    // ev.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    // ev.style.boxShadow = "0px 0px 30px rgb(255, 255, 255)";
+    ev.classList.add("flash")
     timeOut(ev.id);
 }
 timeOut = (sound) =>{
     setTimeout(() => {
         selected = document.getElementById(sound);
-        selected.style.backgroundColor = "rgb(20, 20, 20)";
-        selected.style.boxShadow = "none";
-    }, 200);
+        // selected.style.backgroundColor = "rgb(20, 20, 20)";
+        // selected.style.boxShadow = "none";
+        selected.classList.remove("flash")
+    }, 300);
 };
 buttons.forEach((button) =>{
     button.addEventListener("click", () =>{
